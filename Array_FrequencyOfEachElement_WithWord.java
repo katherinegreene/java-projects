142c6c652846569752e578e24a3badf9f03a6d4e
Sena Greene <kgreene7@my.brookdalecc.edu>
2026-01-18 23:01:28 -0500

    Array_FrequencyOfEachElement_WithWord

---
+++ Array_FrequencyOfEachElement_WithWord.java
@@ -0,0 +1,15 @@
+    /*
+     int[]arr = {1,2,3,4,3,2,1,3,2,2,2,4};
+
+write a program that should count each number and prints a sentence to say how many times a number is repeated example:
+        1 is two,
+        3 is three,
+        4 is two.
+        2 is five
+
+     */
+
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
